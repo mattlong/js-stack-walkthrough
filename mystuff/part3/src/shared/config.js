5f56d5b0298fc509ae2e6af42b242fb052bf9d58
Matt Long <matt.long@plangrid.com>
2017-03-26 23:14:45 -0700

    [Part 6] Add Helmet

--- mystuff/part3/src/shared/config.js
+++ mystuff/part3/src/shared/config.js
@@ -2,7 +2,7 @@
 
 export const WEB_PORT = process.env.PORT || 8000
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'Hello World App'
+export const APP_NAME = 'Fuck Yeah'
 
 export const WDS_PORT = 7000
 export const APP_CONTAINER_CLASS = 'js-app'
